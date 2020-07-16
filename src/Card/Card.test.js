b2fe0e369601078c08e89cdd3e492c5c620f8adb
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-16 11:40:44 -0600

    :bar_chart: completed card test: render card

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -1,5 +1,31 @@
 import React from 'react'
 import Card from '../Card/Card'
 import "@testing-library/jest-dom"
-import { render, fireEvent } from "@testing-library/react"
+import { render, fireEvent, getByText } from "@testing-library/react"
 
+const mockReservationCard = {
+  id: 1,
+  name: "Melissa V",
+  date: "12/12/12",
+  time: "07:40",
+  number: 47,
+}
+
+describe("Card", () => {
+  it('Should render a Card correctly', () => {
+    const { getByText } = render( <Card {...mockReservationCard} />)
+
+    const name = getByText("Melissa V")
+    const date = getByText("12/12/12")
+    const time = getByText("07:40")
+    
+    expect(name).toBeInTheDocument()
+    expect(date).toBeInTheDocument()
+    expect(time).toBeInTheDocument()
+  })
+
+  it('should cancel a reservation', () => {
+    const mockDeleteBtn = jest.mock()
+    const { getByAltText } = render( <Card {...mockReservationCard} />)
+  })
+})
